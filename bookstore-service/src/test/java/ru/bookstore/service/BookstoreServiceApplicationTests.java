a9f5bed0089d46ba0bf1d0e8008f585f224a9ad2
Ivan_Agafonov1 <sudden1306@gmail.com>
2021-06-09 11:15:19 +0300

    update structure

---
+++ bookstore-service/src/test/java/ru/bookstore/service/BookstoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bookstore.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
