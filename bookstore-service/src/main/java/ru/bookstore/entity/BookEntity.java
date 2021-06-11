640ead37243bf1b517d3fa8d9e7a41ad75df7b86
Ivan_Agafonov1 <sudden1306@gmail.com>
2021-06-11 14:00:01 +0300

    Add jsonb field for BookEntity

--- bookstore-service/src/main/java/ru/bookstore/entity/BookEntity.java
+++ bookstore-service/src/main/java/ru/bookstore/entity/BookEntity.java
@@ -1,24 +1,34 @@
 package ru.bookstore.entity;
 
+import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
 import ru.bookstore.model.Book;
 
 import javax.persistence.*;
 import java.util.Objects;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
 
 @Entity
+@TypeDef(name = "jsonb", typeClass = JsonBinaryType.class)
 @Table(name="Books")
 public class BookEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @Embedded
     private Book book;
+
+    @Column(columnDefinition = "jsonb")
+    @Type(type = "jsonb")
+    private Book bookJson;
 
     public BookEntity() {}
 
     public BookEntity(long id, Book book) {
         this.id = id;
         this.book = book;
+        this.bookJson = book;
     }
 
     public long getId() {
@@ -35,6 +45,7 @@ public class BookEntity {
 
     public BookEntity setBook(Book book) {
         this.book = book;
+        this.bookJson = book;
         return this;
     }
 
