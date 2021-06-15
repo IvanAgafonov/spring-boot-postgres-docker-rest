874c711fdf2df7436caa4ad1d47e6fa477e44d01
Ivan_Agafonov1 <sudden1306@gmail.com>
2021-06-15 17:50:29 +0300

    Fix repositories

--- bookstore-service/src/main/java/ru/bookstore/repository/CustomizedBook.java
+++
@@ -1,9 +0,0 @@
-package ru.bookstore.repository;
-
-import ru.bookstore.entity.BookEntity;
-
-import java.util.List;
-
-public interface CustomizedBook {
-    List<BookEntity> findAll();
-}
--- bookstore-service/src/main/java/ru/bookstore/repository/CustomizedBookCrudRepository.java
+++ bookstore-service/src/main/java/ru/bookstore/repository/CustomizedBookCrudRepository.java
@@ -3,5 +3,8 @@ package ru.bookstore.repository;
 import ru.bookstore.entity.BookEntity;
 import org.springframework.data.repository.CrudRepository;
 
-public interface CustomizedBookCrudRepository
-        extends CrudRepository<BookEntity, Long>, CustomizedBook {}
+import java.util.List;
+
+public interface CustomizedBookCrudRepository extends CrudRepository<BookEntity, Long> {
+    List<BookEntity> findAll();
+}
